384043f0c8f187b4ec7692f9f70122e62885c801
amitshinde1616 <amitshinde08.in@gmail.com>
2025-04-20 23:06:58 +0530

    Create script.js

---
+++ my-portfolio/script.js
@@ -0,0 +1,4 @@
+document.querySelector('a[href="#contact"]').addEventListener('click', function(e) {
+    e.preventDefault();
+    document.querySelector('#contact').scrollIntoView({ behavior: 'smooth' });
+});
